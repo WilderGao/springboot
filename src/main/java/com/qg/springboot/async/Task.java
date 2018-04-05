7b36adf21e89c0725f2d11d8986ed46fb980f634
WilderGao <845956752@qq.com>
2018-04-05 12:23:22 +0800

    使用@Async 实现异步调用

---
+++ src/main/java/com/qg/springboot/async/Task.java
@@ -0,0 +1,47 @@
+package com.qg.springboot.async;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * @author:Wilder Gao
+ * @time:2018/4/5
+ * @Discription：使用@Component 说明这个类已经被 spring 管理了
+ */
+@Component
+public class Task {
+    public static Random random = new Random();
+
+    /**
+     * 任务一，异步调用要使用Async注解
+     */
+    @Async
+    public void doTaskOne() throws InterruptedException {
+        System.out.println("开始执行任务一："+new Date());
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(1000));
+        long end = System.currentTimeMillis();
+        System.out.println("任务一完成，一共花费时间："+(end - start) + "毫秒");
+    }
+
+    @Async
+    public void doTaskTwo() throws InterruptedException {
+        System.out.println("开始执行任务二："+new Date());
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(1000));
+        long end = System.currentTimeMillis();
+        System.out.println("任务二完成，一共花费时间："+(end - start) + "毫秒");
+    }
+
+    @Async
+    public void doTaaskThree() throws InterruptedException {
+        System.out.println("开始执行任务三"+new Date());
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(1000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务三，耗时：" + (end - start) + "毫秒");
+    }
+}
---
+++ src/test/java/com/qg/springboot/AsyncTest.java
@@ -0,0 +1,33 @@
+package com.qg.springboot;
+
+import com.qg.springboot.async.Task;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author:Wilder Gao
+ * @time:2018/4/5
+ * @Discription：
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AsyncTest {
+    @Autowired
+    private Task task;
+
+    @Test
+    public void task() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        System.out.println("开始执行所有任务......");
+        task.doTaaskThree();
+        task.doTaskOne();
+        task.doTaskTwo();
+        Thread.sleep(2000);
+        System.out.println("所有任务都执行完毕......");
+        long end = System.currentTimeMillis();
+        System.out.println("所用时间为:"+(end - start)+"毫秒");
+    }
+}
